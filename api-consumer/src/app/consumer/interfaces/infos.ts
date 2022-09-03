5d0f79c5d19aa6dedf67c3ec12b7cc4dc744ecfc
acrucha <mcaa@cin.ufpe.br>
2022-09-03 18:02:00 -0300

    (feat): creating infos interface

---
+++ api-consumer/src/app/consumer/interfaces/infos.ts
@@ -0,0 +1,11 @@
+
+export interface Infos {
+    activity_amount: number,
+    average_duration_per_case: string,
+    average_events_per_case: number,
+    case_amount: number,
+    event_amount: number,
+    events_per_case: string,
+    median_duration: string,
+    variant_amount: number    
+}

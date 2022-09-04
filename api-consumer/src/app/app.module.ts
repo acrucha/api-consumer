86cf23114ec6531f50607825849139cd32ad74ba
acrucha <mcaa@cin.ufpe.br>
2022-09-03 21:31:10 -0300

    (chore): adding imports to app module

--- api-consumer/src/app/app.module.ts
+++ api-consumer/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConsumerComponent } from './consumer/components/consumer.component';
+import { ConsumerFacade } from './consumer/consumer.facade';
+import { ConsumerService } from './consumer/services/consumer.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,10 @@ import { ConsumerComponent } from './consumer/components/consumer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ConsumerService, ConsumerFacade],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

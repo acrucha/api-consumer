a603427c487fd2aadafad366ec3e40ff764d0ee1
acrucha <mcaa@cin.ufpe.br>
2022-09-03 21:41:16 -0300

    (feat): creating consumer component functions

--- api-consumer/src/app/consumer/components/consumer.component.ts
+++ api-consumer/src/app/consumer/components/consumer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ConsumerFacade } from '../consumer.facade';
+import { Infos, defaultInfos } from '../interfaces/infos';
 
 @Component({
   selector: 'app-consumer',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsumerComponent implements OnInit {
 
-  constructor() { }
+  private image: any;
+  private infos: Infos;
 
-  ngOnInit(): void {
+  constructor(private consumerFacade: ConsumerFacade, private sanitizer: DomSanitizer) {
+    this.image = "";
+    this.infos = defaultInfos;
+  }
+
+  ngOnInit(): void {}
+
+  transformHTML(image: string) {
+    return this.sanitizer.bypassSecurityTrustHtml(image);
+  }
+
+  setImage(unidade: number, detail_level: number) {
+    this.consumerFacade.getImage(unidade, detail_level).then(image => {
+      this.image = this.transformHTML(image);
+    }).catch(err => console.log(err.message));
+  }
+
+  getImage(): any {
+    return this.image;
+  }
+
+  setInfos(unidade_id: number) {
+    this.consumerFacade.getInfos(unidade_id).then(infos => {
+      this.infos = infos;
+    }).catch(err => console.log(err.message));
+  }
+
+  getInfos(): Infos {
+    return this.infos;
   }
 
 }
